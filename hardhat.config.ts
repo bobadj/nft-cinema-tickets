67cb7c4eb620d88600c90b9a5999fce008b4b87f
bobadj <slobodan.dj2@gmail.com>
2022-09-15 20:03:44 +0200

    hardhat basic setup

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,27 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import * as dotenv from "dotenv";
+dotenv.config()
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.17",
+  solidity: {
+    version: "0.8.9",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 1000,
+      },
+    },
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCHAN_API_KEY
+  },
+  networks: {
+    goerli: {
+      url: `https://eth-goerli.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY}`,
+      accounts: [process.env.GOERLI_PRIVATE_KEY]
+    }
+  }
 };
 
 export default config;
